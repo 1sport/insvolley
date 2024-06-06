5e6b0a241be164658846509e4390d8efa2794753
Abdubek <zholay.abdubek@gmail.com>
2024-06-06 23:18:04 +0500

    fix texts

--- src/app/_sections/LeaguesList.tsx
+++ src/app/_sections/LeaguesList.tsx
@@ -2,14 +2,14 @@ export const LeaguesList = () => {
   return (
     <div className="bg-primary-200 h-[420px] flex flex-col">
       <div className="bg-primary-150 flex items-center justify-center uppercase font-extrabold text-xl py-3">
-        Список лиги
+        Турнирная таблица
       </div>
       <div className="flex w-full">
         <div className="bg-gradient-to-r from-[#1B5C98] to-[#2E9AFE] flex-1 flex items-center justify-center py-3">
-          Республиканский
+          Нац. Лига (М)
         </div>
         <div className="bg-[#2369D3] flex-1 flex items-center justify-center py-3">
-          Мировой
+          Нац. Лига (Ж)
         </div>
       </div>
     </div>
--- src/app/_sections/Rating.tsx
+++ src/app/_sections/Rating.tsx
@@ -2,14 +2,14 @@ export const Rating = () => {
   return (
     <div className="bg-primary-200 h-[420px] flex flex-col">
       <div className="bg-primary-150 flex items-center justify-center uppercase font-extrabold text-xl py-3">
-        Рейтинг
+        Рейтинг сборных
       </div>
       <div className="flex w-full">
         <div className="bg-gradient-to-r from-[#1B5C98] to-[#2E9AFE] flex-1 flex items-center justify-center py-3">
-          Республиканский
+          Мужчины
         </div>
         <div className="bg-[#2369D3] flex-1 flex items-center justify-center py-3">
-          Мировой
+          Женщины
         </div>
       </div>
     </div>
